be93d27d538911289fb43305e34b9dabe55061ea
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-07-16 10:35:16 -0400

    initial code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log('linked');
+$(() => {
+  const $container = $('#container');
+  //   C O D E
+  $('#btn-info').on('click', () => {
+    const $div = $('<div>').addClass('display');
+    const $form = $('<form>').addClass('form');
+    const $input = $('<input>').attr('type', 'text');
+    const $btnSearch = $('<button>')
+      .addClass('info-btn')
+      .text('Search');
+    const $btnRandom = $('<button>')
+      .addClass('info=btn')
+      .text('Random');
+    $form.append($input, $btnSearch, $btnRandom);
+    $div.append($form);
+    $container.append($div);
+  });
+});
